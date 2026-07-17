85f25a906c8b906477ebc75999ee5d23c192efa6
Abigael Nyakara <abbynyakara@gmail.com>
2026-07-17 13:34:18 -0400

    Added incident reports restraint staging

---
+++ ciyj_analytics/models/staging/stg_incident_reports_restraint.sql
@@ -0,0 +1,39 @@
+{{ config(materialized='view') }}
+
+WITH source AS (
+    SELECT *
+    FROM {{ source('ciyj_raw', 'incident_reports_restraint') }}
+)
+
+SELECT
+    -- Identifiers
+    ID                                               AS incident_id,
+    VERSIONNUMBER                                    AS version_number,
+    COLLECTIONPERIOD                                 AS collection_period,
+    RESPONDENTID                                     AS respondent_id,
+    TRY_TO_TIMESTAMP(CREATED)                        AS created,
+
+    -- Period
+    TRY_TO_DATE(PERIODBEGIN, 'YYYY-MM-DD')          AS period_begin,
+    TRY_TO_DATE(PERIODEND, 'YYYY-MM-DD')            AS period_end,
+
+    -- Facility dimensions
+    FACILITYURBANRURAL                               AS facility_urban_rural,
+    FACILITYFIELDAVERAGENAME                         AS facility_field_avg_name,
+    FACILITYREGION                                   AS facility_region,
+
+    -- Incident metadata
+    ROWNUMBER                                        AS row_number,
+    TRY_TO_DATE(OCCURRED, 'YYYY-MM-DD')              AS occurred_date,
+    OCCURREDINLIVINGUNIT                             AS occurred_in_living_unit,
+
+    -- Youth involved
+    YOUTHID                                          AS youth_id,
+    GENDER                                           AS gender,
+    TRY_TO_NUMBER(AGE)                               AS age,
+    ETHNICITY                                        AS ethnicity,
+
+    -- Restraint details
+    RESTRAINTTYPE                                    AS restraint_type
+
+FROM source
